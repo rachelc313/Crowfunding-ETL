9d16d2f93f7ba45ac21ddba144cedd97b8ce33bd
rachelc313 <111570965+rachelc313@users.noreply.github.com>
2022-10-25 13:53:36 -0500

    reformatting folders and adding code

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -43,4 +43,33 @@ CREATE TABLE "contacts" (
 		"contact_id"
      )
 );
+--DROP TABLE "backers";
 
+CREATE TABLE "backers" (
+	"backer_id" varchar(10) NOT NULL,
+	"cf_id" int NOT NULL,
+	"first_name" varchar(50) NOT NULL,
+	"last_name" varchar(50) NOT NULL,
+	"email" varchar(100) NOT NULL,
+	CONSTRAINT "pk_backers" PRIMARY KEY (
+		"backer_id"
+	)
+);
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_cf_id" FOREIGN KEY("cf_id")
+REFERENCES "backers" ("cf_id");
+
+SELECT * FROM "backers";
+SELECT * FROM "campaign";
+SELECT * FROM "category";
+SELECT * FROM "contacts";
+SELECT * FROM "subcategory";
